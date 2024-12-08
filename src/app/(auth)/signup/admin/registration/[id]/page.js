9c7d0667577a9340c19c051c728733e7fcf3b392
bandasatwik07 <110599397+bandasatwik07@users.noreply.github.com>
2024-12-08 19:07:13 +0530

    Delete src directory

--- src/app/(auth)/signup/admin/registration/[id]/page.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import AdminRegistration from "@/components/Signup/AdminRegistration/AdminRegistration";
-const Registration = () => {
-  return <AdminRegistration />;
-};
-
-export default Registration;
--- src/app/(auth)/signup/student/registration/[id]/page.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import StudentRegistration from "@/components/Signup/StudentRegistration/StudentRegistration";
-const Registration = () => {
-  return <StudentRegistration />;
-};
-
-export default Registration;
